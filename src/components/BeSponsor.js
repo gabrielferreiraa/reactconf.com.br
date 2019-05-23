d676d8328bc77c34980fb1c1bcac15cb42e958fd
Gabriel Ferreira <gabriel@lightup.com.br>
2019-05-23 15:25:43 -0300

    creating meetups data js

--- src/components/BeSponsor.js
+++ src/components/BeSponsor.js
@@ -1,92 +1,22 @@
-import React, { PureComponent } from 'react';
-import { css } from 'glamor';
+import React from 'react';
+import styled from 'styled-components';
 import Globals from '../utils/Globals';
+import Button from './Button';
 
-//import Text from './Text';
+const Container = styled.div`
+  background: ${Globals.colors.white};
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+`;
 
-//import englishthingImg from '../media/images/sponsors/englishthing.png';
-
-const styles = {
-  container: css({
-    background: Globals.colors.white,
-    width: '100vw',
-    alignItems: 'center',
-    '@media(max-width: 720px)': {
-      alignSelf: 'auto',
-    },
-  }),
-  sponsorbtn: css({
-    color: Globals.colors.white,
-    fontSize: 20,
-    textAlign: 'center',
-    maxWidth: 200,
-    borderRadius: 4,
-    padding: '10px 20px',
-    backgroundColor: '#FA46FF',
-    border: `1px solid #FA46FF`,
-    display: 'block',
-    margin: '0 auto 4em auto',
-    textDecoration: 'none',
-    ':not(.is-disabled):hover': {
-      backgroundColor: Globals.colors.white,
-      border: `1px solid #FA46FF`,
-      color: '#FA46FF',
-    },
-  }),
-  paragraphAfterParty: css({
-    textAlign: 'center',
-    ':first-letter': {
-      textTransform: 'uppercase',
-    },
-  }),
-  link: css({
-    color: '#666666',
-  }),
-  card: css({
-    width: 250,
-    height: 150,
-    '@media(min-width: 721px)': {
-      height: 100,
-    },
-    padding: '0',
-    backgroundColor: Globals.colors.white,
-    borderRadius: 0,
-    margin: 10,
-    '> a > img': {
-      maxWidth: 250,
-      maxHeight: 150,
-      margin: '0 auto',
-      display: 'block',
-    },
-  }),
-  cards: css({
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
-    width: '100%',
-    maxWidth: 1000,
-    margin: '80px auto',
-  }),
-};
-
-class BeSponsor extends PureComponent {
-  render() {
-    return (
-      <div {...styles.container}>
-        <p>
-          <a
-            {...styles.sponsorbtn}
-            href={`mailto:${Globals.contacts.email}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Seja um patrocinador
-          </a>
-        </p>
-      </div>
-    );
-  }
-}
+const BeSponsor = () =>
+  <Container>
+    <Button href={`mailto:${Globals.contacts.email}`}>
+      Seja um patrocinador
+    </Button>
+  </Container>;
 
 export default BeSponsor;
---
+++ src/utils/Meetups.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: 1,
+    name: 'Meetup Salvador/BA',
+    link: 'https://www.facebook.com/reactssa/',
+  },
+  {
+    id: 2,
+    name: 'Meetup Belo Horizonte/MG',
+    link: 'https://www.meetup.com/reactbh/',
+  },
+];
